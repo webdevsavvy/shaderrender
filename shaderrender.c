b3033e0930c1228b505f7660985ee58002a5c552
webdevsavvy <devmail.adrianabreu@gmail.com>
2025-06-06 19:07:21 +0100

    clean up some of the io interaction

--- shaderrender.c
+++ shaderrender.c
@@ -40,32 +40,32 @@
 #define READ_END 0
 #define WRITE_END 1
 
-#define STR_EQ(__s1, __s2) (strcmp(__s1, __s2) == 0)
+#define streq(__s1, __s2) (strcmp(__s1, __s2) == 0)
 
 // Global variable declarations
 //------------------------------------------------------------------------------------------
 
 size_t screenWidth = 1200; // screen width in pixels
 size_t screenHeight = 800; // screen height in pixels
-size_t fps = 60; // number of frames per second
+size_t fps = 60;           // number of frames per second
 
-char *outputModeArg = "window"; // argument for the type of rendering output (default `window`)
-char *fragShaderArg; // argument for the relative path to fragment shader file
-char *vertShaderArg; // argument for the relative path to vertex shader file
-int durationArg = 1; // argument for the duration of video output
+char *outputModeArg = NULL; // argument for the type of rendering output (default `window`)
+char *fragShaderArg;        // argument for the relative path to fragment shader file
+char *vertShaderArg;        // argument for the relative path to vertex shader file
+int durationArg = 1;        // argument for the duration of video output
 
 bool fragShaderFlag = false; // flag set if fragment shader argument is passed
 bool vertShaderFlag = false; // flag set if vertex shader argument is passed
-bool durationFlag = false; // flag set if duration of video output argument is passed
+bool durationFlag = false;   // flag set if duration of video output argument is passed
 
 // Function declarations
 //------------------------------------------------------------------------------------------
 
 /**
  * @brief Parse the CLI arguments and set the global flags and arguments.
- * 
- * @param argc 
- * @param argv 
+ *
+ * @param argc
+ * @param argv
  */
 void ParseProgramArguments(int argc, char **argv);
 
@@ -85,7 +85,7 @@ void RunVideoRenderer(const char *windowTitle);
 
 /**
  * @brief Print the usage for this program.
- * 
+ *
  */
 void PrintProgramUsage(char *programName);
 
@@ -100,80 +100,100 @@ Shader LoadShaderAndSetResolution(const char *vertPath, const char *fragPath);
 
 /**
  * @brief Program main entry point.
- * 
- * @param argc 
- * @param argv 
- * @return int 
+ *
+ * @param argc
+ * @param argv
+ * @return int
  */
 int main(int argc, char **argv)
 {
     ParseProgramArguments(argc, argv);
 
-    if (strcmp(outputModeArg, "window") == 0) {
-        printf("[shader-renderer] Running Windowed Renderer with options: fragment shader %s\n", fragShaderArg);
-        RunWindowedRenderer("shader-renderer");
-        return 0;
+    if (outputModeArg == NULL)
+    {
+        printf("ERROR: The argument -m <output-mode> was not provided!\n");
+        PrintProgramUsage(argv[0]);
+        exit(1);
     }
 
-    if (strcmp(outputModeArg, "video") == 0) {
-        printf("[shader-renderer] Running Video Renderer with options: fragment shader %s\n", fragShaderArg);
-        RunVideoRenderer("shader-renderer");
-        return 0;
+    if (!fragShaderFlag && ! vertShaderFlag) {
+        printf("ERROR: The arguments -f <fragment-shader> and -v <vertex-shader> are missing!\nAt least one of them must be set.\n");
+        PrintProgramUsage(argv[0]);
+        exit(1);
     }
 
-    return 1;
+    if (streq(outputModeArg, "window"))
+    {
+
+        RunWindowedRenderer("shaderrender");
+        exit(0);
+    } 
+    
+    else if (streq(outputModeArg, "video"))
+    {
+        RunVideoRenderer("shaderrender");
+        exit(0);
+    }
+
+    else
+    {
+        printf("ERROR: The argument -m <output-mode> was not recognized!\n'%s' is not a valid output mode, please use one of these ('window', 'video')\n", outputModeArg);
+        PrintProgramUsage(argv[0]);
+        exit(1);
+    }
 }
 
-void PrintProgramUsage(char *programName) 
+void PrintProgramUsage(char *programName)
 {
-    printf("Usage: %s [-f <fragment_shader>] [-v <vertex_shader>] [-m <output_mode>] [-d <duration>]\n", programName);
-    printf("\t|Argument|\t\t\t|Description|\n");
-    printf("\t<fragment_shader>\t\tRelative path to the fragment shader code file\n");
-    printf("\t<vertex_shader>\t\t\tRelative path to the vertex shader code file\n");
-    printf("\t<output_mode>\t\t\tOutput mode for the shader rendering (window | video, default: window)\n");
-    printf("\t<duration>\t\t\tDuration of video output in seconds\n");
+    printf("Usage: %s [-f <fragment-shader>] [-v <vertex-shader>] [-m <output-mode>] [-d <duration>]\n", programName);
+    printf("\t-f <fragment-shader>\t\tRelative path to the fragment shader code file\n");
+    printf("\t-v <vertex-shader>\t\tRelative path to the vertex shader code file\n");
+    printf("\t-m <output-mode>\t\tOutput mode for the shader rendering (window | video, default: window)\n");
+    printf("\t-d <duration>\t\t\tDuration of video output in seconds\n");
 }
 
 void ParseProgramArguments(int argc, char **argv)
 {
-    if (argc == 1)
-    {
-        PrintProgramUsage(argv[0]);
-        exit(0);
-    }
-
     for (int i = 0; i < argc; i++)
     {
         char *arg = argv[i];
 
-        if (STR_EQ(arg, "-m"))
+        if (streq(arg, "--"))
+        {
+            break;
+        }
+
+        if (streq(arg, "-m"))
         {
             outputModeArg = argv[i + 1];
-            printf("[shader-renderer] Setting enabled: output mode %s\n", outputModeArg);
             continue;
         }
 
-        if (strcmp(arg, "-f") == 0)
+        if (streq(arg, "-f"))
         {
             fragShaderArg = argv[i + 1];
             fragShaderFlag = true;
-            printf("[shader-renderer] Setting enabled: fragment shader %s\n", argv[i + 1]);
             continue;
         }
 
-        if (strcmp(arg, "-d") == 0)
+        if (streq(arg, "-d"))
         {
             durationArg = atoi(argv[i + 1]);
             durationFlag = true;
-            printf("[shader-renderer] Setting enabled: duration %d\n", durationArg);
             continue;
+        }
+
+        if (streq(arg, "-h"))
+        {
+            PrintProgramUsage(argv[0]);
+            exit(0);
         }
     }
 }
 
 void RunWindowedRenderer(const char *windowTitle)
 {
-    SetTraceLogLevel(LOG_INFO);
+    SetTraceLogLevel(LOG_NONE);
 
     SetConfigFlags(FLAG_WINDOW_UNDECORATED);
 
@@ -185,8 +205,7 @@ void RunWindowedRenderer(const char *windowTitle)
 
     Shader shader = LoadShaderAndSetResolution(
         vertShaderFlag ? vertShaderArg : 0,
-        fragShaderFlag ? fragShaderArg : 0
-    );
+        fragShaderFlag ? fragShaderArg : 0);
 
     float time = 0.0f;
     int timeLoc = GetShaderLocation(shader, "u_time");
@@ -202,19 +221,18 @@ void RunWindowedRenderer(const char *windowTitle)
 
         if (IsKeyPressed(KEY_R))
         {
-            printf("[shader-renderer] Reloading shader ...\n");
+            printf("[shaderrender] Reloading shader ...\n");
 
             UnloadShader(shader);
 
             shader = LoadShaderAndSetResolution(
                 vertShaderFlag ? vertShaderArg : 0,
-                fragShaderFlag ? fragShaderArg : 0
-            );
+                fragShaderFlag ? fragShaderArg : 0);
 
             // Update time location (might have changed in the new shader)
             timeLoc = GetShaderLocation(shader, "u_time");
 
-            printf("[shader-renderer] Shader reloaded successfully\n");
+            printf("[shaderrender] Shader reloaded successfully\n");
         }
 
         SetShaderValue(shader, timeLoc, &time, SHADER_UNIFORM_FLOAT);
@@ -229,14 +247,14 @@ void RunWindowedRenderer(const char *windowTitle)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadShader(shader);        // Unload shader
+    UnloadShader(shader); // Unload shader
 
     // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
     CloseWindow();
 }
 
-void RunVideoRenderer(const char *windowTitle) 
+void RunVideoRenderer(const char *windowTitle)
 {
     // int ffmpeg = ffmpeg_start_rendering(screenWidth, screenHeight, fps);
     FFMPEG *ffmpeg = InitFFMPEG(screenWidth, screenHeight, fps);
@@ -272,14 +290,13 @@ void RunVideoRenderer(const char *windowTitle)
         SetShaderValue(shader, timeLoc, &time, SHADER_UNIFORM_FLOAT);
         BeginDrawing();
         ClearBackground(WHITE); // Clear screen background
-        
 
         BeginShaderMode(shader);
         DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK);
         EndShaderMode();
-        
+
         EndDrawing();
-        
+
         Image image = LoadImageFromScreen();
         SendFrameFFMPEG(ffmpeg, image.data, screenWidth, screenHeight);
         UnloadImage(image);
@@ -287,7 +304,7 @@ void RunVideoRenderer(const char *windowTitle)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    UnloadShader(shader);        // Unload shader
+    UnloadShader(shader); // Unload shader
     UnloadFFMPEG(ffmpeg);
 
     // Close window and OpenGL context
